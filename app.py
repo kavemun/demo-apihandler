ab1cfde18f13014b2387e0b0ef939b3aef779986
kavemun <kahmun@gmail.com>
2018-04-24 00:46:48 +0800

    Fixed error with handlers

--- app.py
+++ app.py
@@ -3,16 +3,10 @@ import tornado.ioloop
 import tornado.web
 import tornado.httpclient
 
-from handler import BaseHandler
-from apihandler import APIHandler
-
 from handler import default_handlers
 
-
 if __name__ == "__main__":
-    app = tornado.web.Application([
-        (r"/foo/?", default_handlers),
-    ])
+    app = tornado.web.Application(default_handlers)
     app.listen(8888)
 
     tornado.ioloop.IOLoop.current().start()
